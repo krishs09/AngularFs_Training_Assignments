4d1e3d9ef0aefc5f8ae8e5d86af0366d8c6b7248
Krishna Yadav <krishs09@yahoo.in>
2022-04-25 14:46:03 +0530

    Update Demo1.ts

--- Typescript/Day1/Demo1.ts
+++ Typescript/Day1/Demo1.ts
@@ -1,2 +1,11 @@
 let str = " Welcome to typescript";
 console.log(str);
+
+let id=  6465656;
+let uname = "John";
+let job = "Manager";
+let isEmployeePermenet=true;
+let salary = "INR"+.45000;
+let deptNo= 10;
+
+console.log(id+ " "+uname+" "+job+" "+isEmployeePermenet+" "+salary+ " "+deptNo);
