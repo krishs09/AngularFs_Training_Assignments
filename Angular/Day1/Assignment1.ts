90fd6c14ec2af8d4b21fcb8c84c66dff27b61ed0
Krishna Yadav <krishs09@yahoo.in>
2022-04-27 14:32:34 +0530

    Update Assignment1.ts

--- Angular/Day1/Assignment1.ts
+++ Angular/Day1/Assignment1.ts
@@ -1,1 +1,21 @@
 
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-emp',
+  templateUrl: './emp.component.html',
+  styleUrls: ['./emp.component.css']
+})
+export class EmpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public empId:number  = 1;
+  public empName:string  = "Krishna";
+  public job:string  = "Work";
+  public salary:number = 213;
+  public depo:number = 11;
+}
