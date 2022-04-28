348a25b1e99f291e5edde0410d13387b272dc49d
Krishna Yadav <krishs09@yahoo.in>
2022-04-28 17:28:19 +0530

    Create sort-options.component.ts

---
+++ Angular/Day2/Assignment2/sort-options.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sort-options',
+  templateUrl: './sort-options.component.html',
+  styleUrls: ['./sort-options.component.css']
+})
+export class SortOptionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
