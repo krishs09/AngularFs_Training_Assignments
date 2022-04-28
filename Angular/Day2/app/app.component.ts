3c9f64891cafe975f09bc5f303eea328426bab86
Krishna Yadav <krishs09@yahoo.in>
2022-04-28 15:14:50 +0530

    Update app.component.ts

--- Angular/Day2/app/app.component.ts
+++ Angular/Day2/app/app.component.ts
@@ -10,14 +10,15 @@ export class AppComponent {
   productsArray=[
     {pName:"Nokia",pCategory:"Mobiles",pPrice:"777"},
     {pName:"Table",pCategory:"Furniture",pPrice:"44"},
-    {pName:"Washin Mashine",pCategory:"Home Appliance",pPrice:"33"},
+    {pName:"Washin Mashine",pCategory:"Home Appliances",pPrice:"33"},
     {pName:"Samsung",pCategory:"Mobiles",pPrice:"88"},
     {pName:"Bed",pCategory:"Furniture",pPrice:"11"},
   ];
 
   getProductList(category:any){
     console.log(category);
-    console.log(this.productsArray.filter(item=>item.pCategory=category));
+    console.log(this.productsArray.filter(item=>item.pCategory==category));
+    this.productsArray = this.productsArray.filter(item=>item.pCategory==category);
   }
 
 }
