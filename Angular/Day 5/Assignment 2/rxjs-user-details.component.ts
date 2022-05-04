d702a3192a2fe63780bcf2f5d8b9694e546313dc
Krishna Yadav <krishs09@yahoo.in>
2022-05-04 18:54:18 +0530

    Update rxjs-user-details.component.ts

--- Angular/Day 5/Assignment 2/rxjs-user-details.component.ts
+++ Angular/Day 5/Assignment 2/rxjs-user-details.component.ts
@@ -1,1 +1,27 @@
 
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { RxjsServiceService } from '../rxjs-service.service';
+
+@Component({
+  selector: 'app-rxjs-demo',
+  templateUrl: './rxjs-demo.component.html',
+  styleUrls: ['./rxjs-demo.component.css']
+})
+export class RxjsDemoComponent implements OnInit {
+userDetails:any[] = [];
+
+  constructor(private rxjsService:RxjsServiceService) { }
+
+  ngOnInit(): void {
+  }
+
+  getData(){
+    this.rxjsService.getData().subscribe((response:any)=>{
+      console.log(response);
+      this.userDetails = response.data;
+    });
+  }
+
+
+}
